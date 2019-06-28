6ed3169229fb7fb47c37b52723db50a058624212
Sachini <sachini.mc@gmail.com>
2019-06-27 20:50:17 -0700

    Pepper resources

---
+++ Pepper/README.md
@@ -0,0 +1,14 @@
+# Pepper 
+
+Slides: [Introduction](https://docs.google.com/presentation/d/1uViDWhsALzFUFQwSkuy10qRR7-YLyCX1iUiI09kJ4nA/edit?usp=sharing)
+
+## Resources
+
+<ul>
+* ChatBot
+	* [QiChat concepts](http://doc.aldebaran.com/2-1/naoqi/audio/dialog/dialog-syntax_full.html)
+	* [Pepper Animation List](http://doc.aldebaran.com/2-4/naoqi/motion/alanimationplayer-advanced.html)
+* Box objects
+	* [Input/ output notations](http://doc.aldebaran.com/2-4/software/choregraphe/objects/box_input_output.html)
+	* [Adding parameters, input, output](http://doc.aldebaran.com/1-14/software/choregraphe/objects/box_optional_components.html)
+</ul>
---
+++ Pepper/robot_greeter/translations/translation_en_US.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Okay, I know you now! See you later then.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Okay, I know you now! See you later then.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Learn Face/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Done</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Done</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
